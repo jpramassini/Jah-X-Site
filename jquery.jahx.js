f323efcfeb3ddc2c4b50483375407d39ca208022
Unknown <jpramassini@gmail.com>
2017-11-06 20:17:12 -0500

    Framework for Page System in Place - Adding Content
    Added Page system framework with divs and implemented smooth scrolling with Jquery. Still no mobile menu yet.

--- jquery.jahx.js
+++ jquery.jahx.js
@@ -1,5 +1,5 @@
 $(function(){
-  //Blur backgroun video on scroll
+  //Blur background video on scroll
   $(window).on('scroll', function(){
 
       var s = $(window).scrollTop(),
@@ -10,12 +10,36 @@ $(function(){
          "ms-filter":"blur("+blurVal +"px)",
          "o-filter":"blur("+blurVal +"px)",
          "filter":"blur("+blurVal +"px)"});
-  });
+  }); //End of blur method
 
-  $(".toggle-nav").on("click", function(){
+  //Method to toggle menu cluster on click
+  $(".toggler").on("click", function(){
 
     $(".menu").toggle();
 
+  });//end toggle method
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
-  //end of Blur function
+
 });
